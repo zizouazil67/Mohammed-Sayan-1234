c3857c0d4f478ed76ade26899746297572144547
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-11 08:29:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,53 +32,86 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
-const fs = require("fs");
-client.on('message', async message =>{
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+let channel =     client.channels.get("470237074344378368")
+setInterval(function() {
+channel.send(`**السلام عليكم ارجوا منكم دخول هذا السيرفر
+لو انت مطفر ادخل لربح المال https://discord.gg/J5hT6eV **`);
+}, 7200000)
+})
+
+
+
+
+
+
+
+
+
+
+const fs = require("fs"); //npm i fs
+const ms = require("ms"); //npm i ms
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+client.on('message', message =>{
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-    let xp = require("./xp.json");
+    let prefix = '!!';
+     
+    if(cmd === `${prefix}warn`) {
 
-  let xpAdd = Math.floor(Math.random() * 7) + 8;
-  console.log(xpAdd);
+  //!warn @daeshan <reason>
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("U don't have enough permissions to warn Users!");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("yo i can't find this User");
+  if(wUser.hasPermission("ADMINISTRATOR")) return message.reply("This User is very cool why warn him? >.>");
+  let reason = args.join(" ").slice(22);
 
-  if(!xp[message.author.id]){
-    xp[message.author.id] = {
-      xp: 0,
-      level: 1
-    };
-  }
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
 
+  warns[wUser.id].warns++;
 
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 300;
-  xp[message.author.id].xp =  curxp + xpAdd;
-  if(nxtLvl <= xp[message.author.id].xp){
-    xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.RichEmbed()
-    .setTitle("Level Up!")
-    .setColor(purple)
-    .addField("New Level", curlvl + 1);
-
-    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
-  }
-  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
-    if(err) console.log(err)
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
   });
+
+  let warnEmbed = new Discord.RichEmbed()
+  .setDescription("Warns")
+  .setAuthor(message.author.username)
+  .setColor("#fc6400")
+  .addField("Warned User", `<@${wUser.id}>`)
+  .addField("Warned In", message.channel)
+  .addField("Number of Warnings", warns[wUser.id].warns)
+  .addField("Reason", reason);
+
+
+  message.channel.send(warnEmbed);
+
+  if(warns[wUser.id].warns == 2){
+    let muterole = message.guild.roles.find(`name`, "muted");
+    if(!muterole) return message.reply("You should make A **muted** role, to mute this User!!");
+
+    let mutetime = "10s";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`<@${wUser.id}> Just muted for sometime!`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.reply(`<@${wUser.id}> Just unmuted!`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 3){
+    message.guild.member(wUser).ban(reason);
+    message.reply(`<@${wUser.id}> Just banned for 3 warns!!`)
+  }
+
+}
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
