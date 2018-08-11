12b16d67673b41bc21d1370c0e75bafa374eeac5
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-11 09:29:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -514,7 +514,6 @@ client.on('guildMemberRemove', member => {
 
 
 
-const Discord = require('discord.js');
 const db = require('quick.db');
 const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
