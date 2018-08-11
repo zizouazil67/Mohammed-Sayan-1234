208e028b15ef50f2bcf46d4b89267c8ae0fbf9e0
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-11 09:27:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,8 +32,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
 
 
 
-const Discord = require("discord.js")
-const client = new Discord.Client()
 client.on("ready", () => {
 let channel =     client.channels.get("470237074344378368")
 setInterval(function() {
