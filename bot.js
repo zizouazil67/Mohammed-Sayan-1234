4e0228c1091c656960765b34a90f7355decf4a07
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-12 07:39:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,6 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 
 const prefix = "!!";
-client.on('guildMemberRemove', member => {
-client.on('guildMemberAdd', member => {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -282,17 +280,42 @@ For additional help,  `)
 
 
 
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('477713932388139053').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('477713823357468683').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+
+
+
+
+client.on('message', message => {
+     if(message.content.startsWith(prefix + "clear")) {
+         var args = message.content.split(" ").slice(1);
+ if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You need MANAGE_MESSAGES permission noob');
+  if (!args[0]) return message.channel.send('You didn\'t provide any number!!!');
+
+  message.channel.bulkDelete(args[0]).then(() => {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xF16104)
+      .setDescription(`Cleared ${args[0]} messages.`);
+    message.channel.send({ embed });
+
+    const actionlog = message.guild.channels.find('name', 'logs');
+
+    if (!actionlog) return message.channel.send('Can\'t find action-log channel. Are you sure that this channel exists and I have permission to view it? **CANNOT POST LOG.**');
+    const embedlog = new Discord.RichEmbed()
+      .setDescription('~Purge~')
+      .setColor(0xF16104)
+      .addField('Purged By', `<@${message.author.id}> with ID ${message.author.id}`)
+      .addField('Purged in', message.channel)
+      .addField('Time', message.createdAt);
+    actionlog.send(embedlog);
+   
+  });
+};
+
 });
 
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('477713932388139053').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('477713823357468683').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
-});
+
+
+
+
 
 
 
