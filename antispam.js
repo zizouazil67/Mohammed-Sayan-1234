ead4572b8b04a26814680be23681ea423058caf2
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-13 08:27:32 +0300

    Create antispam.js

---
+++ antispam.js
@@ -0,0 +1,24 @@
+let message_handler = {};
+    let tolerancia = [4, 3500]; //4 messages in less than 1200ms = spam
+    module.exports = function(bot, callback){
+      bot.on('message', message => {
+        if(message.author.bot) return;
+        if(!message_handler.hasOwnProperty(message.author.id)){
+          message_handler[message.author.id] = {};
+          message_handler[message.author.id].ultimamessageTS = message.createdTimestamp;
+          message_handler[message.author.id].primeiramessageTS = message.createdTimestamp;
+          message_handler[message.author.id].messageTracker = 0;
+        }
+        var mh = message_handler[message.author.id];
+        mh.messageTracker++;
+        mh.ultimamessageTS = message.createdTimestamp;
+        if(mh.messageTracker >= tolerancia[0] && mh.ultimamessageTS <= (mh.primeiramessageTS + tolerancia[1])){
+            callback(message);
+        } else {
+          setTimeout(function(){
+            delete message_handler[message.author.id];
+            delete mh;
+          }, tolerancia[1]);
+        }
+      });
+    }
