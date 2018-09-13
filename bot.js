5fc1dc107972898044684974ba87f134e035f888
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-09-13 09:36:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = [`${client.guilds.size} Server`,'G.help','Type G.help',`${client.users.size} Members`,'!help','By Mohamed | MohamedSamaYT'];
+    var setGame = [`${client.guilds.size} Server ','!help','Type !help',`${client.users.size} Members`,'!help','By Mohamed | MohamedSamaYT'];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -205,6 +205,46 @@ if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send
     })
     }
 }); 
+
+var port = process.env.PORT || 3000;
+
+const bot = new Discord.Client({disableEveryone: false, autoReconnect:true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./cmds/", (err, files) => {
+    if(err) console.error(err)
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands were found!");
+        return;
+    }
+    console.log(`Loading ${jsfiles.length} commands!`);
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} has succesfully booted!`);
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" "); 
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot, message, args);
+});
+
 var attentions = {};
 var times = {
     "1⃣": "m",
